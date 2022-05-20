af549499209208d5aaf34044121107a6337d7983
Leandro Peixoto de Oliveira <loliveira@gasbrasiliano.com.br>
2022-05-20 17:18:08 -0300

    Ajustes query sql

--- inventario.py
+++ inventario.py
@@ -26,6 +26,7 @@ class ContentNavigationDrawer(Screen):
 class Principal(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.atual_dialog = None
         self.label_descr = None
         self.icone = None
         self.num_invent = None
@@ -42,11 +43,10 @@ class Principal(Screen):
 
     def inserir(self):
         # Buscar dados para alimentar o aplicativo
-        print(len(self.manager.get_screen('pesquisar').resultado))
 
         # Organizar os dados na tela do app
         for index, linha in enumerate(self.manager.get_screen('pesquisar').resultado):
-            self.ids.swiper.clear_widgets()
+            self.lista.append([])
             self.insere_swiper = MDSwiperItem()  # Criar um "swiper" para cada imobilizado
             self.ids.swiper.add_widget(self.insere_swiper)
 
@@ -65,24 +65,28 @@ class Principal(Screen):
             self.num_imob = MDTextField(text=linha[0], pos_hint={'x': 0.15, 'y': .72},
                                         size_hint=(.7, .02), halign='center')
             self.inserir_layout.add_widget(self.num_imob)
-
+            self.lista[index].append(self.num_imob)
             self.insere_denom = MDTextField(text=linha[1], pos_hint={'x': 0.15, 'y': .55},
                                             size_hint=(.7, .1), halign='center')
             self.inserir_layout.add_widget(self.insere_denom)
-
+            self.lista[index].append(self.insere_denom)
             self.num_invent = MDTextField(text=str(linha[2]).zfill(6), pos_hint={'x': 0.2, 'y': .4},
                                           size_hint=(.5, .1), hint_text='Nº Inventário', mode="rectangle")
             self.inserir_layout.add_widget(self.num_invent)
-            self.lista.append(self.num_invent)
+            self.lista[index].append(self.num_invent)
             self.num_serie = MDTextField(text=str(linha[3]), pos_hint={'x': 0.2, 'y': .25},
                                          size_hint=(.5, .1), hint_text="Nº Série", mode="rectangle")
             self.inserir_layout.add_widget(self.num_serie)
-
+            self.lista[index].append(self.num_serie)
             self.icone = MDIconButton(icon='microphone', icon_size='33sp', pos_hint={'x': 0.71, 'y': .39})
             self.lista_icon.append(self.icone)
             self.inserir_layout.add_widget(self.lista_icon[index])
             self.lista_icon[index].bind(on_press=partial(self.ouvir_numero, index))
+            self.ident = MDLabel(text=str(linha[4]))
+            self.lista[index].append(self.ident)
 
+        self.insere_swiper2 = MDSwiperItem()
+        self.ids.swiper.add_widget(self.insere_swiper2)
         self.ids.swiper.set_current(1)
 
     def ouvir_numero(self, index, instance):
@@ -98,8 +102,18 @@ class Principal(Screen):
             print('não entendi')
 
     def gravar(self):
-        pass
+        conn = sqlite3.connect('base')
+        cursor = conn.cursor()
 
+        for imob in self.lista:
+            cursor.execute('UPDATE inventario set Imobilizado = ?, Denominação = ?, Inventario = ?, Serie = ? where '
+                           '[index] = ?', (imob[0].text, imob[1].text, imob[2].text, imob[3].text, int(imob[4].text)))
+
+        conn.commit()
+        conn.close()
+        
+        self.atual_dialog = MDDialog(text="Registro alterado com sucesso!", radius=[20, 7, 20, 7], )
+        self.atual_dialog.open()
 
 class TelaPesquisa(Screen):
     def __init__(self, **kwargs):
@@ -111,26 +125,27 @@ class TelaPesquisa(Screen):
         conn = sqlite3.connect('base')
         cursor = conn.cursor()
 
-        cursor.execute('select Imobilizado, Denominação, Inventario, Serie from inventario where Classe like '
-                       'case when ? != "" then ? else "IES-%" END and Data >= case when ? != "" '
-                       'then ? else "2000-01-01 00:00:00" end and Data <= case when ? != ""'
-                       'then ? else "2100-01-01 00:00:00" end',
-                       (self.ids.classe.text, self.ids.classe.text, datetime.datetime.strptime(self.ids.data_ini.text,
-                        "%d/%m/%Y") if self.ids.data_ini.text != '' else '',
-                        datetime.datetime.strptime(self.ids.data_ini.text, "%d/%m/%Y") if self.ids.data_ini.text != ''
-                        else '', datetime.datetime.strptime(self.ids.data_fim.text, "%d/%m/%Y") if
-                        self.ids.data_ini.text != '' else '',
-                        datetime.datetime.strptime(self.ids.data_fim.text, "%d/%m/%Y") if self.ids.data_ini.text != ''
-                        else ''))
+        cursor.execute(
+            'select Imobilizado, Denominação, Inventario, Serie, [index] from inventario where Denominação like case '
+            'when ? != "" then ? else "%" end and Classe like '
+            'case when ? != "" then ? else "IES-%" END and Data >= case when ? != "" '
+            'then ? else "2000-01-01 00:00:00" end and Data <= case when ? != ""'
+            'then ? else "2100-01-01 00:00:00" end and Inventario like case when ? != "" then ? else "%" end',
+            (self.ids.descricao.text, "%" + self.ids.descricao.text + "%", self.ids.classe.text, self.ids.classe.text,
+             datetime.datetime.strptime(self.ids.data_ini.text, "%d/%m/%Y") if self.ids.data_ini.text != ''
+             else '', datetime.datetime.strptime(self.ids.data_ini.text, "%d/%m/%Y") if
+             self.ids.data_ini.text != '' else '', datetime.datetime.strptime(self.ids.data_fim.text, "%d/%m/%Y") if
+             self.ids.data_ini.text != '' else '',
+             datetime.datetime.strptime(self.ids.data_fim.text, "%d/%m/%Y") if self.ids.data_ini.text != ''
+             else '', self.ids.num_invent.text, self.ids.num_invent.text))
 
         self.resultado = cursor.fetchall()
-        print(self.ids.classe.text)
-        print(self.resultado)
         if len(self.resultado) == 0:
             self.erro_dialog = MDDialog(text="A pesquisa não retornou resultados!", radius=[20, 7, 20, 7], )
             self.erro_dialog.open()
         else:
             self.manager.current = 'principal'
+        conn.close()
 
 
 class WindowManager(ScreenManager):
